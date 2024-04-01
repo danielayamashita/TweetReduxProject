b1a0f141faf2c260492521b0c1ca93493bdc2489
daniy <daniyamashita@gmail.com>
2024-04-01 20:43:28 +0200

    Implemented the reducers

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,45 @@
-const App = () => {
-  return <div>Starter Code</div>;
+import { connect} from "react-redux";
+import { useEffect, Fragment  } from "react";
+import { Route, Routes } from "react-router-dom";
+import { handleInitialData } from "../actions/shared";
+import Dashboard from "./Dashboard";
+import LoadingBar from "react-redux-loading-bar";
+import NewTweet from "./NewTweet";
+import TweetPage from "./TweetPage";
+import Nav from "./Nav";
+
+const App = (props) => {
+  useEffect(() => {
+    props.dispatch(handleInitialData());
+  }, []);
+
+  return (
+    <div>
+      <Fragment>
+        
+        <LoadingBar />
+        <div className="container">
+          <Nav/>
+          {
+            props.loading === true ? null :(
+              <Routes>
+                <Route path="/" exact element ={<Dashboard/>}/>
+                <Route path="/tweet/:id" element={<TweetPage/>}/>
+                <Route path="/new" element={<NewTweet/>}/>
+              </Routes>
+            )
+          }
+        </div>
+      </Fragment>
+
+      
+      
+    </div>
+  );
 };
 
-export default App;
+const mapStateToProps = ({ authedUser }) => ({
+  loading: authedUser === null,
+});
+
+export default connect(mapStateToProps)(App);
